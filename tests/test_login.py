371a877d108fc19778edee3f8e914463535eab81
preetygoyal <preetygoyalb1990@gmail.com>
2026-05-15 10:43:54 +0100

    run browser headless in ci

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,7 +5,7 @@ def test_login():
 
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
 
         page = browser.new_page()
 
