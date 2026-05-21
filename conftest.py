6c676284576135afa975d9f2bf1e0e30fc985984
preetygoyal <preetygoyalb1990@gmail.com>
2026-05-21 11:40:57 +0100

    Run Playwright in headless mode for CI

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def page(request):
 
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         yield page
